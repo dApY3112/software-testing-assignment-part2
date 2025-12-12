debac98f9b45e3ee13af467ee20718a1fae1f7e3
Tin <nguyentritin211@gmail.com>
2025-12-12 11:39:48 +0200

    Return normal

--- src/isBuffer.js
+++ src/isBuffer.js
@@ -1,32 +1,15 @@
 import root from './.internal/root.js'
 
 /** Detect free variable `exports`. */
-// const freeExports = typeof exports === 'object' && exports !== null && !exports.nodeType && exports
-
-// /** Detect free variable `module`. */
-// const freeModule = freeExports && typeof module === 'object' && module !== null && !module.nodeType && module
-
-// /** Detect the popular CommonJS extension `module.exports`. */
-// const moduleExports = freeModule && freeModule.exports === freeExports
-// /** Built-in value references. */
-// const Buffer = moduleExports ? root.Buffer : undefined
-// /* Built-in method references for those with the same name as other `lodash` methods. */
-// const nativeIsBuffer = Buffer ? Buffer.isBuffer : undefined
-
-const freeExports = typeof exports !== 'undefined' && typeof exports === 'object' && exports !== null && !exports.nodeType && exports
-
+const freeExports = typeof exports === 'object' && exports !== null && !exports.nodeType && exports
 /** Detect free variable `module`. */
-const freeModule = typeof module !== 'undefined' && freeExports && typeof module === 'object' && module !== null && !module.nodeType && module
-
+const freeModule = freeExports && typeof module === 'object' && module !== null && !module.nodeType && module
 /** Detect the popular CommonJS extension `module.exports`. */
 const moduleExports = freeModule && freeModule.exports === freeExports
-
 /** Built-in value references. */
 const Buffer = moduleExports ? root.Buffer : undefined
-
 /* Built-in method references for those with the same name as other `lodash` methods. */
 const nativeIsBuffer = Buffer ? Buffer.isBuffer : undefined
-
 /**
  * Checks if `value` is a buffer.
  *
