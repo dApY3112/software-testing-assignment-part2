af9312a8f7d49932bce966bc1e019f8f471c8a72
ad1426 <113592444+ad1426@users.noreply.github.com>
2025-12-12 21:28:20 +0700

    Adjust to exclude files dont need

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,4 +1,17 @@
 export default {
   testEnvironment: "node",
-  testMatch: ["**/tests/**/*.test.js"],
+  
+  testMatch: [
+    "<rootDir>/tests/other-utils/countBy.test.js",
+    "<rootDir>/tests/other-utils/difference.test.js",
+    "<rootDir>/tests/other-utils/drop.test.js",
+    "<rootDir>/tests/other-utils/filter.test.js",
+    "<rootDir>/tests/other-utils/get.test.js",
+    "<rootDir>/tests/other-utils/isEmpty.test.js",
+    "<rootDir>/tests/other-utils/isObjectLike.test.js",
+    "<rootDir>/tests/other-utils/toNumber.test.js",
+
+    "<rootDir>/tests/self-designed/add.test.js",
+    "<rootDir>/tests/self-designed/capitalize.test.js"
+  ]
 };
