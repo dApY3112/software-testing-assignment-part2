2fd9fef0ef74df13700e0849a6e7b73487823968
Tin <nguyentritin211@gmail.com>
2025-12-12 11:19:07 +0200

    command error

--- src/isBuffer.js
+++ src/isBuffer.js
@@ -1,13 +1,13 @@
 import root from './.internal/root.js'
 
-/** Detect free variable `exports`. */
-const freeExports = typeof exports === 'object' && exports !== null && !exports.nodeType && exports
+// /** Detect free variable `exports`. */
+// const freeExports = typeof exports === 'object' && exports !== null && !exports.nodeType && exports
 
-/** Detect free variable `module`. */
-const freeModule = freeExports && typeof module === 'object' && module !== null && !module.nodeType && module
+// /** Detect free variable `module`. */
+// const freeModule = freeExports && typeof module === 'object' && module !== null && !module.nodeType && module
 
-/** Detect the popular CommonJS extension `module.exports`. */
-const moduleExports = freeModule && freeModule.exports === freeExports
+// /** Detect the popular CommonJS extension `module.exports`. */
+// const moduleExports = freeModule && freeModule.exports === freeExports
 
 /** Built-in value references. */
 const Buffer = moduleExports ? root.Buffer : undefined
