970d5986ab6efae965ee41c0d46527bb2eb0a9a2
Tin <nguyentritin211@gmail.com>
2025-12-12 12:56:10 +0200

    Test123

---
+++ tests/other-utils/isBuffer.test.js
@@ -0,0 +1,16 @@
+// import isBuffer from "../../src/isBuffer.js";
+
+// describe("isBuffer", () => {
+//   test("returns false even for Node Buffer in this environment (implementation-specific)", () => {
+//     expect(isBuffer(Buffer.from("abc"))).toBe(false);
+//   });
+
+//   test("returns false for Uint8Array", () => {
+//     expect(isBuffer(new Uint8Array([1, 2, 3]))).toBe(false);
+//   });
+
+//   test("returns false for null/undefined", () => {
+//     expect(isBuffer(null)).toBe(false);
+//     expect(isBuffer(undefined)).toBe(false);
+//   });
+// });
